25d751841d24e441feb4514b4e61059b8608aea3
duongtien <duongtien291102@gmail.com>
2025-08-20 17:11:04 +0700

    them logo

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import { Button, ButtonGroup } from "react-bootstrap";
+import logoCompany from './logocongan.svg';
 
 const Header: React.FC = () => {
   return (
     <div className="d-flex justify-content-between align-items-center px-3 py-2 border-bottom text-white">
       {/* Logo + Title */}
       <div className="d-flex align-items-center">
-        {/* Logo (nếu có) */}
-        {/* <img
-          src="/logo192.png"
+        <img
+          src={logoCompany}
           alt="Logo"
           style={{ width: "40px", height: "40px", marginRight: "10px" }}
-        /> */}
+        />
         <h5 className="m-0 fw-bold text-uppercase" style={{ color: "#000" }}>
           Thống kê tình hình{" "}
           <span className="text-danger">Cháy, Nổ, CNCH</span>
@@ -25,15 +25,14 @@ const Header: React.FC = () => {
     border: '1px solid #000',
     padding: '3px',
     display: 'inline-block',
-    borderRadius: '6px',
     fontWeight: 500,
     color: '#000',
     fontSize: '14px',
     textAlign: 'center',
     cursor: 'pointer',
-    margin: '0 10px',
     boxShadow: '0 2px 4px rgba(0,0,0,0.1)',
     minWidth: '400px',
+    marginTop: '0px',
   }}
 >
   <ButtonGroup className="d-flex gap-2 align-items-center" style={{ width: '100%' }}>
